b03c75f30fd5dffee9b104d5ae7c1e3f6dec688a
debela1606 <yilikalafework@gmail.com>
2022-03-11 04:35:58 -0800

    commit 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main- Entry point
+ * main-Entry point
  * return: Always 0 (success)
  */
 int main (void)
@@ -10,4 +10,5 @@ printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 
 }
+
 

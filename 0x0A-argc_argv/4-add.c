e1cd19ccd7ff16296d1065124770b34620d57fee
debela1606 <yilikalafework@gmail.com>
2022-04-05 08:56:50 -0700

    a program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,37 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints the sum of the whole numbers passed to this program
- * @argc: the number of command-line
+ * main - Prints the sum of the whole numbers passed to this program
+ * @argc: The number of command-line arguments
  * @argv: The command-line arguments
- * 
- * Return: 0 if succcessfull, otherwise 1
+ *
+ * Return: 0 if successfull, otherwise 1
  */
 int main(int argc, char *argv[])
 {
-        int i,j, sum = 0;
-        if (argc > 1)
-	{
-                   for (i = 1; i < argc; i++)
-                   {
-                          for(j = 0;argv[i][j]; j++)
-                               if(argv[i][j] < '0' ||  argv[i][j] > '9')
-                                           return (printf("Error\n"), 1);
-                          sum += atoi(argv[i]);
+	int num, i, j;
+	int sum = 0;
 
-                   }
-	}
-	else
+	for (i = 1; i < argc; i++)
 	{
-	printf("%i\n", sum);
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (argv[i][j] >= '0' && argv[i][j] <= '9')
+			{
+				j++;
+			}
+			else
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+		num = atoi(argv[i]);
+		/* sscanf(argv[i], "%d", &num); */
+		sum += num;
 	}
+	printf("%d\n", sum);
 	return (0);
-
 }

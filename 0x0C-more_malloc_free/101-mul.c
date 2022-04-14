2819b2d4fd086d91336c6a4b43533cf632b6ecd1
debela1606 <yilikalafework@gmail.com>
2022-04-14 05:36:53 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,8 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * _is_zero - determines if any number is zero
  * @argv: argument vector.
